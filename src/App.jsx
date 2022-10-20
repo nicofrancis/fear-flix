41c6bd6390d1491e75316a21949f9448d0ec1b78
nicofrancis <nicolofrancis10@gmail.com>
2022-10-20 16:03:21 -0600

    backend up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState(null);
+
+  const getMovies = () => {
+    const options = {
+      method: "GET",
+      url: "https://streaming-availability.p.rapidapi.com/get/basic",
+      params: { country: "us", tmdb_id: "movie/120", output_language: "en" },
+      headers: {
+        "X-RapidAPI-Key": "4352a3c067mshb2c5424d4de88ffp1bc3e6jsn125fd08dd6b7",
+        "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setMovies(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  console.log("movies", movies);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <div className="App">
